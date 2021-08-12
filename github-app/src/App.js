141289c251b67ed8151d5e950b7f60d56163afc3
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2021-08-12 19:14:00 -0300

    estilizando os componentes com styled-component

--- github-app/src/App.js
+++ github-app/src/App.js
@@ -1,31 +1,17 @@
 import React from 'react';
 import Layout from './components/layout';
+import Profile from './components/profile';
 
 function App() {
-  return <main>
-    <Layout>
-      <div>
-        <img source="https://avatars.githubusercontent.com/u/54314671?v=4" alt="Avatar of user"/>
-        <h1>Carlos Vinícius</h1>
-        <h3>Username: </h3>
-        <span>vinicarlosss</span>
-        <div>
-          <h4>Followers</h4>
-          <span>5</span>
-        </div>
-        <div>
-          <h4>Starreds</h4>
-          <span>5</span>
-        </div>
-        <div>
-          <h4>Followings</h4>
-          <span>5</span>
-        </div>
-      </div>
-      <div>Repositories</div>
-      <div>Starreds</div>
-    </Layout>
-  </main>;
+  return (
+  <main>
+      <Layout>
+        <Profile/>
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+  </main>
+  );
 }
 
 export default App;
---
+++ github-app/src/components/profile/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import * as S from './styled';
+
+const Profile = () => {
+    return <S.Wrapper>
+        <S.WrapperImage
+            src="https://avatars.githubusercontent.com/u/54314671?v=4" 
+            alt="Avatar of user"
+        />
+        <S.WrapperInfoUser>
+            <div>
+                <h1>Carlos Vinícius</h1>
+                <S.WrapperUsername>
+                    <h3>Username: </h3>
+                    <span>vinicarlosss</span>
+                </S.WrapperUsername>
+            </div>
+            <S.WrapperStatusCount>
+                <div>
+                    <h4>Followers</h4>
+                    <span>5</span>
+                </div>
+                <div>
+                    <h4>Starreds</h4>
+                    <span>5</span>
+                </div>
+                <div>
+                    <h4>Followings</h4>
+                    <span>5</span>
+                </div>
+            </S.WrapperStatusCount>
+        </S.WrapperInfoUser>
+    </S.Wrapper>
+};
+
+export default Profile;
